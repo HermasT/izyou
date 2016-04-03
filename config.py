c8859b65e13db14312f6485303e64edba80ac375
hermas <tangsh@ctrip.com>
2016-04-04 07:44:33 +0800

    course register

--- config.py
+++ config.py
@@ -10,11 +10,9 @@ RECAPTCHA_PUBLIC_KEY = '6Lfol9cSAAAAADAkodaYl9wvQCwBMr3qGR_PPHcw'
 # connect string for local postgresql server
 #SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://root@localhost/izyou'
 # connect string for rds mysql server
-#SQLALCHEMY_DATABASE_URI = 'mysql://izyou:ZYJizyou@rdsvdk0918cl7z915n71.mysql.rds.aliyuncs.com/izyou'
-
+# SQLALCHEMY_DATABASE_URI = 'mysql://izyou:ZYJizyou@rdsvdk0918cl7z915n71.mysql.rds.aliyuncs.com/izyou'
 # connect string for local mysql server
-# SQLALCHEMY_DATABASE_URI = 'mysql://root@localhost/izyou'
-SQLALCHEMY_DATABASE_URI = 'mysql://root:111111@localhost/izyou'
+SQLALCHEMY_DATABASE_URI = 'mysql://root:123456@localhost/izyou'
 
 #SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
 #WHOOSH_BASE = os.path.join(basedir, 'search.db')
